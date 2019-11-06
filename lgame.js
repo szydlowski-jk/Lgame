36b93d8bee8eab156be98a2102532e7b66a9969c
Jacek Szydlowski <szydlowski.jk@gmail.com>
2019-11-06 22:58:25 +0100

    first board display

--- lgame.js
+++ lgame.js
@@ -1,9 +1,65 @@
 'use strict'
 
+
 class LGame {
     constructor () {
-        this.ls = [
+        this.redl = [1, 2, 6, 10]
+        this.bluel = [5, 9, 13, 14]
+        this.neutral = [0, 15]
+    }
 
-        ]
+    draw(gc) {
+        const ctx = gc.getContext('2d')
+        const size = gc.width;
+
+        const cellSize = gc.width / 4
+
+        for(let i = 0; i < 16; i++) {
+            if (this.bluel.includes(i)) {
+                ctx.fillStyle = "#0000ff"
+            } else if (this.redl.includes(i)) {
+                ctx.fillStyle = "#ff0000"
+            } else if (this.neutral.includes(i)) {
+                ctx.fillStyle = "#880088"
+            } else {
+                ctx.fillStyle = "#000000"
+            }
+
+            ctx.strokeStyle = "#ffffff"
+            let ix = i % 4
+            let iy = Math.floor(i / 4)
+            ctx.fillRect(
+                ix * cellSize,
+                iy * cellSize,
+                cellSize,
+                cellSize
+            )
+            ctx.strokeRect(
+                ix * cellSize,
+                iy * cellSize,
+                cellSize,
+                cellSize
+            )
+        }
+    }
+
+    print() {
+        let str = ""
+        for(let i = 0; i < 16; i++) {
+            if (this.bluel.includes(i)) {
+                str += '+'
+            } else if (this.redl.includes(i)) {
+                str += '-'
+            } else if (this.neutral.includes(i)) {
+                str += 'X'
+            } else {
+                str += '.'
+            }
+
+            if ((i % 4) == 0) {
+                console.log(str)
+                str = ""
+            }
+        }
     }
 }
--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 'use strict'
 
+let game = new LGame;
 
 function handleResize() {
     const gc = document.getElementById('gc')
     const ctx = gc.getContext('2d')
 
     let size = Math.min( window.innerWidth , window.innerHeight )
-    gc.width = Math.floor( size * 0.8 );
-    gc.height = Math.floor( size * 0.8 );
+    gc.width = Math.floor( (size * 0.8 ) / 4) * 4;
+    gc.height = Math.floor( ( size * 0.8 ) / 4) * 4;
+    game.draw(gc);
 }
 
 window.onload = function() {
     this.handleResize()
     window.onresize = handleResize;
+
 }
